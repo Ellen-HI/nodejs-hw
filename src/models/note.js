0a0945c6dbc42727a7ae85d83dd394e3a6dba1e5
HELEN <alenagolosova97@gmail.com>
2026-08-21 20:16:38 +0300

    add connect mongodb

---
+++ src/models/note.js
@@ -0,0 +1,39 @@
+import { Schema, model } from 'mongoose';
+
+const noteSchema = new Schema(
+  {
+    title: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: false,
+      trim: true,
+      default: '',
+    },
+    tag: {
+      type: String,
+      enum: [
+        'Work',
+        'Personal',
+        'Meeting',
+        'Shopping',
+        'Ideas',
+        'Travel',
+        'Finance',
+        'Health',
+        'Important',
+        'Todo',
+      ],
+      required: false,
+      default: 'Todo',
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const Note = model('Note', noteSchema);
