8d9c79dc1ba8ad55ad6e7d0170c060409c552255
HELEN <alenagolosova97@gmail.com>
2026-08-28 14:37:19 +0300

    fix port, add errors in the server

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { errors } from 'celebrate';
 import cors from 'cors';
 import 'dotenv/config';
 
@@ -9,7 +10,7 @@ import { notFoundHandler } from './middleware/notFoundHandler.js';
 import notesRoutes from './routes/notesRoutes.js';
 
 const app = express();
-const PORT = process.env.PORT ?? 3030;
+const PORT = process.env.PORT ?? 3000;
 
 app.use(logger);
 app.use(express.json());
@@ -17,7 +18,9 @@ app.use(cors());
 
 app.use(notesRoutes);
 
+app.use(errors());
 app.use(notFoundHandler);
+
 app.use(errorHandler);
 
 await connectMongoDB();
