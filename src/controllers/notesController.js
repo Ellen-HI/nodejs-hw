60143368f8ad0fe2aec511a2a4d3b93c97f9adb8
HELEN <alenagolosova97@gmail.com>
2026-08-28 14:21:51 +0300

    add constants, validations

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -1,17 +1,42 @@
 import { Note } from '../models/note.js';
 import createHttpError from 'http-errors';
-
 export const getAllNotes = async (req, res, next) => {
-  try {
-    const notes = await Note.find();
-    res.status(200).json(notes);
-  } catch (err) {
-    next(err);
+  const { page = 1, perPage = 15, tag, search } = req.query;
+  const skip = (page - 1) * perPage;
+
+  const myQuery = Note.find();
+
+  if (search) {
+    myQuery.where({
+      $or: [
+        { title: { $regex: search, $options: 'i' } },
+        { content: { $regex: search, $options: 'i' } },
+      ],
+    });
   }
+
+  if (tag) {
+    myQuery.where('tag').equals(tag);
+  }
+
+  const [totalNotes, notes] = await Promise.all([
+    myQuery.clone().countDocuments(),
+    myQuery.skip(skip).limit(perPage),
+  ]);
+
+  const totalPages = Math.ceil(totalNotes / perPage);
+
+  res.status(200).json({
+    page,
+    perPage,
+    totalNotes,
+    totalPages,
+    notes,
+  });
+
 };
 
 export const getNoteById = async (req, res, next) => {
-
   try {
     const { noteId } = req.params;
     const note = await Note.findById(noteId);
@@ -46,7 +71,6 @@ export const deleteNote = async (req, res, next) => {
 };
 
 export const updateNote = async (req, res, next) => {
-
   try {
     const { noteId } = req.params;
     const note = await Note.findOneAndUpdate({ _id: noteId }, req.body, {
