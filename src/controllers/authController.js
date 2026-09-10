36ed9e0ee4021ccb0107d66c9761f4715f665f2f
HELEN <alenagolosova97@gmail.com>
2026-09-10 19:04:42 +0300

    add users/me/avatar

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,6 +3,9 @@ import { User } from '../models/user.js';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import { sendEmail } from '../utils/sendMail.js';
+import handlebars from 'handlebars';
+import path from 'node:path';
+import fs from 'node:fs/promises';
 
 import { createSession, setSessionCookies } from '../services/auth.js';
 import { Session } from '../models/session.js';
@@ -111,12 +114,21 @@ export const requestResetEmail = async (req, res) => {
     { expiresIn: '15m' },
   );
 
+  const templatePath = path.resolve('src/templates/reset-password-email.html');
+  const templateSource = await fs.readFile(templatePath, 'utf-8');
+  const template = handlebars.compile(templateSource);
+
+  const html = template({
+    name: user.username,
+    link: `${process.env.FRONTEND_DOMAIN}/reset-password?token=${resetToken}`,
+  });
+
   try {
     await sendEmail({
       from: process.env.SMTP_FROM,
       to: email,
       subject: 'Reset your password',
-      html: `<p>Click <a href="${process.env.FRONTEND_DOMAIN}/reset-password?token=${resetToken}">here</a> to reset your password!</p>`,
+      html,
     });
   } catch {
     throw createHttpError(
@@ -127,5 +139,28 @@ export const requestResetEmail = async (req, res) => {
 
   res.status(200).json({
     message: 'Password reset email sent successfully',
+  });
+};
+
+export const resetPassword = async (req, res) => {
+  const { password, token } = req.body;
+  let payload;
+
+  try {
+    payload = jwt.verify(token, process.env.JWT_SECRET);
+  } catch {
+    throw createHttpError(401, 'Invalid or expired token');
+  }
+
+  const user = await User.findOne({ _id: payload.sub, email: payload.email });
+  if (!user) {
+    throw createHttpError(404, 'User not found');
+  }
+
+   const hashedPassword = await bcrypt.hash(password, 10);
+  await User.updateOne({ _id: user._id }, { password: hashedPassword });
+  await Session.deleteMany({ userId: user._id });
+  res.status(200).json({
+    message: 'Password reset successfully',
   });
 };
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import dns from 'node:dns';
+dns.setServers(['8.8.8.8', '1.1.1.1']);
 import express from 'express';
 import { errors } from 'celebrate';
 import cors from 'cors';
@@ -9,6 +11,7 @@ import { errorHandler } from './middleware/errorHandler.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import notesRoutes from './routes/notesRoutes.js';
 import authRoutes from './routes/authRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -18,6 +21,7 @@ app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
 app.use(authRoutes);
+app.use(userRoutes);
 app.use(notesRoutes);
 
 app.use(errors());
